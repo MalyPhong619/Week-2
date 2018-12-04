61eab2aec5586db81790882e597cc8543f703795
Student <example@example.com>
2018-12-04 09:39:52 -0800

    collect length and width and output as a string if square or not

--- sinata_rectangle_tracker/app.rb
+++ sinata_rectangle_tracker/app.rb
@@ -6,8 +6,13 @@ require('pry')
     erb(:input)
   end
 
-  get('/output')
-    binding.pry
+  get('/output') do
+    length = params.fetch("length")
+    width = params.fetch("width")
+    if length == width
+      @rectangle = "This is a square"
+    else
+      @rectangle = "This is not a square"
+    end
     erb(:output)
   end
-end
